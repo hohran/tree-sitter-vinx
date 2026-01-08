b050bdff978c274c862e4eb29e4db3acda4537cf
Jan Hranicka <hranickajan@seznam.cz>
2026-01-08 23:29:05 +0100

    feat: llm created highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,5 +1,28 @@
-(repeat_quantifier) @function
-(label) @comment
-(value) @keyword
-(variable) @type
+;; comments
 (comment) @comment
+
+;; literals
+(string) @string
+(number) @number
+
+;; identifiers
+(variable) @variable
+(iterator) @variable.builtin
+
+;; keywords / operators
+(repeat_quantifier) @keyword
+(file_load) @function
+(value) @constant
+
+;; optional: directions / effects
+(direction) @constant.builtin
+(effect) @attribute
+(color_name) @constant
+(color_value) @number
+
+; (repeat_quantifier) @function
+; (value) @keyword
+; (iterator) @keyword
+; (variable) @type
+; (string) @string
+; (file_load) @function
